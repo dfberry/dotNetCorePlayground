389eeb18bf54f772f525132955c84909c61dcc72
Dina Berry <dinaberry@outlook.com>
2017-09-27 12:34:38 -0700

    emotion & text translation

---
+++ cog_console/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace cog_console
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //TranslateList().Wait();
+            EmotionFromImage().Wait();
+        }
+
+        private static async Task EmotionFromImage()
+        {
+            string subscriptionKey = "SUBSCRIPTION_KEY";
+            string uri = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize";
+
+            var myAvatar = @"{'url': 'https://secure.gravatar.com/avatar/4f16f89a8a4f01a7525294cd8f1daa8e.jpg?s=512&r=g&d=mm'}";
+            HttpContent contentPost = new StringContent(myAvatar, Encoding.UTF8, "application/json");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.PostAsync(uri, contentPost);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.Write(content);
+        }
+        private static async Task TranslateList()
+        {
+            string subscriptionKey = "SUBSCRIPTION_KEY";
+            string uri = "https://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForTranslate";
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(uri);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.Write(content);
+        }
+    }
+}
